2dea31d20eb58c13e31c19d01228db723f4f4209
Elián Joel Minaya Sánchez <85661199+Eminayas@users.noreply.github.com>
2021-07-11 23:04:19 -0500

    Add files via upload

---
+++ buscador 2.py
@@ -0,0 +1,68 @@
+class Buscador:
+    def __init__(self,dato):
+        self.lista=dato
+        
+        
+    def recorrerElemento(self):
+        for ele in self.lista:   #[::-1] / para colocarlo al reves 
+            print(ele,end=" ")
+        for ele in self.lista[::-1]:
+            print(ele,end=" ")    
+            
+        print()     
+        for pos,ele in enumerate(self.lista):
+            print("[{}]={}  ".format(pos,ele))
+        print()    
+        
+        for pos in range(len(self.lista)-1,0,-1):
+            print("[{}]={}  ".format(pos,self.lista[pos]))     
+    
+    
+    def buscar(self,buscado):
+        encontrado=False
+        for pos,ele in enumerate(self.lista):
+             if ele==buscado:
+                 encontrado=True
+                 break # rompe el for
+                 
+         
+        if encontrado:   return pos
+        else:            return -1 
+        
+    def ordenarAsc(self):
+        for pos in range(0,len(self.lista)-1):
+            for sig in range(pos+1, len(self.lista)):
+                if self.lista[pos] > self.lista[sig]:
+                    aux = self.lista[pos]
+                    self.lista[pos]=self.lista[sig]
+                    self.lista[sig]=aux
+        
+               
+            
+             
+datos = [25,15,20,10]
+bus = Buscador(datos)
+print(bus.lista)
+bus.ordenarAsc()
+print(bus.lista)
+
+
+
+
+#buscado=1
+
+#datos="Hola"
+#        0 1 2 3 4        
+
+#bus.recorrerElemento()
+# valor=1
+# resp= bus.buscar(valor)
+# if resp !=-1: print("el numero:{} se encuentra en la posicion: [{}] de la lista: {}".format(valor,resp,bus.lista))
+# else: print("el numero:{} no se encuentra en la lista: {}".format(valor,bus.lista))
+
+# numerosbuscados = (2,4,6,1)
+# respuestas= {}
+# for valor in numerosbuscados:
+#     resp = bus.buscar(valor)
+#     if resp !=-1: respuestas[valor]=resp
+# print(respuestas)
